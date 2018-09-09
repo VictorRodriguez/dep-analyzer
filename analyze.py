417eb1279f85439169b3b907bca75c013db00678
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-09-09 16:23:39 +0000

    check only log files to avoid duplication of jsons
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ import re
 
 import utils
 import json
+import wget
 
 data = {}
 libraries = []
@@ -13,6 +14,26 @@ yum_conf = "/etc/yum.conf"
 max_lib_cnt = 20
 max_bin_cnt = 20
 
+def get_commit(release,pkg):
+    blame_log = []
+    # get info from :
+    # https://cdn.download.clearlinux.org/releases/24660/clear/RELEASENOTES
+    filename = "/tmp/RELEASENOTES-" + str(release)
+    if not os.path.isfile(filename):
+        url = 'https://cdn.download.clearlinux.org/releases/'+ release+'/clear/RELEASENOTES'
+        filename = wget.download(url=url,out=filename)
+    else:
+        token = "Changes in package " + (pkg)
+        with open(filename) as f:
+            content = f.readlines()
+            for line in content:
+                if token in line:
+                    print(line)
+                    blame_log.append(line.strip())
+                    blame_log.append(content[content.index(line)+1].strip().split("-")[0])
+    tmp = list(set(blame_log))
+    str1 = ''.join(tmp)
+    return str1
 
 def add_binary(binary):
     """Add a binary to the list."""
@@ -67,48 +88,65 @@ def whatprovides(file_name):
 def analysis():
     """Fill the bin/lib lists."""
     for logs in os.listdir(utils.results):
-        log = os.path.abspath(utils.results + logs)
-        benchmark = logs.split(".")[0]
-        lib_count = 0
-        bin_count = 0
-        with open(log) as f:
-            content = f.readlines()
-            for line in content:
-                if "openat" in line or "access" in line:
-                    if "/usr/lib" in line and lib_count < max_lib_cnt:
-                        m = re.search('<(.+?)>', line)
-                        if m:
-                            lib = m.group(1)
-                            add_lib(lib)
-                            lib_count += 1
-                    if "/usr/bin" in line and bin_count < max_bin_cnt:
-                        m = re.search('/usr/bin/(.+?)"', line)
-                        if m:
-                            binary = "/usr/bin/" + m.group(1)
-                            add_binary(binary)
-                            bin_count += 1
+        if logs.endswith(".log"):
+            log = os.path.abspath(utils.results + logs)
+            benchmark = logs.split(".")[0]
+            lib_count = 0
+            bin_count = 0
+            with open(log) as f:
+                content = f.readlines()
+                for line in content:
+                    if "openat" in line or "access" in line:
+                        if "/usr/lib" in line and lib_count < max_lib_cnt:
+                            m = re.search('<(.+?)>', line)
+                            if m:
+                                lib = m.group(1)
+                                add_lib(lib)
+                                lib_count += 1
+                        if "/usr/bin" in line and bin_count < max_bin_cnt:
+                            m = re.search('/usr/bin/(.+?)"', line)
+                            if m:
+                                binary = "/usr/bin/" + m.group(1)
+                                add_binary(binary)
+                                bin_count += 1
 
-        data[benchmark] = []
-        for lib in libraries:
-            # print("Benchmark " + benchmark + " call: " + lib)
-            pkg = whatprovides(lib)
-            if (pkg):
+            data[benchmark] = []
+            # TODO
+            # regresion is hardcoded at this point , please let me knwo whats
+            # thebest way to pass the regression version to the script
+            regression = 24760
+            if regression: 
                 data[benchmark].append({
-                    'lib': lib,
-                    'provided by': pkg
+                    'regresion': regression,
                 })
+            for lib in libraries:
+                # print("Benchmark " + benchmark + " call: " + lib)
+                pkg = whatprovides(lib)
+                if (pkg):
+                    data[benchmark].append({
+                        'lib': lib,
+                        'provided by': pkg
+                    })
+                    blame_log = get_commit(regression,pkg)
+                    if (blame_log):
+                        data[benchmark].append({
+                            'changelog': blame_log,
+                        })
 
-        for binary in binaries:
-            # print("Benchmark " + benchmark + " call: " + binary)
-            pkg = whatprovides(binary)
-            if (pkg):
-                data[benchmark].append({
-                    'binary': binary,
-                    'provided by': pkg
-                })
-
-
-        if data:
-            json_file = "results/" + benchmark + "-data.json"
-            with open(json_file, 'w') as outfile:
-                json.dump(data, outfile)
+            for binary in binaries:
+                # print("Benchmark " + benchmark + " call: " + binary)
+                pkg = whatprovides(binary)
+                if (pkg):
+                    data[benchmark].append({
+                        'binary': binary,
+                        'provided by': pkg
+                    })
+                    blame_log = get_commit(regression,pkg)
+                    if (blame_log):
+                        data[benchmark].append({
+                            'changelog': blame_log,
+                        })
+            if data:
+                json_file = "results/" + benchmark + "-data.json"
+                with open(json_file, 'w') as outfile:
+                    json.dump(data, outfile)
